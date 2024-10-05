849cd909b4fa366ca45a95cd73df990e5cda0b1f
vladimirantonyan7@gmail.com <vladimirantonyan7@gmail.com>
2024-10-05 13:19:12 +0400

    the last updates

---
+++ DatabaseClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace TelegramBot
+{
+    internal class DatabaseClass
+    {
+        protected static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\User\\Documents\\HayqGPTDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+        protected static SqlConnection connection = new SqlConnection(connectionString);
+
+       
+
+        public static void OpenConnection()
+        {
+            connection.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        public static void SqlCommand(string query) 
+        {
+            SqlCommand cmd = new SqlCommand(query,connection);
+            cmd.ExecuteNonQuery();
+        }
+        public static SqlDataReader SqlDataReader(string query) 
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+           
+            return reader;
+        }
+
+        public static SqlConnection ReturnConnection()
+        {
+            return connection;
+        }
+        
+
+
+    }
+}
---
+++ JsonDeserlize.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    public class Chat
+    {
+        public long id { get; set; }
+        public string type { get; set; }
+        public string username { get; set; }
+        public string first_name { get; set; }
+    }
+
+    public class From
+    {
+        public long id { get; set; }
+        public bool is_bot { get; set; }
+        public string first_name { get; set; }
+       
+        public string username { get; set; }
+        public string language_code { get; set; }
+    }
+
+    public class Message
+    {
+        public long message_id { get; set; }
+        public From from { get; set; }
+        public int date { get; set; }
+        public Chat chat { get; set; }
+        public string text { get; set; }
+        
+      
+    }
+
+    public class Root
+    {
+        public long update_id { get; set; }
+        public Message message { get; set; }
+
+       
+    }
+
+}
